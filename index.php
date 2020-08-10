3dd4cfd3088b7f1ad33d969a9037101a890a20ce
hassehulabeck <hansbudandersson@gmail.com>
2020-08-10 10:13:03 +0200

    Lite algoritm-tänk...

--- index.php
+++ index.php
@@ -15,6 +15,13 @@ $d4 = new Dice;
 $d5 = new Dice;
 
 $dies = [$d1, $d2, $d3, $d4, $d5];
+// Algoritm för att kolla om vi har ett par.
+/*
+a = 1 och b = (a+1)
+Kolla tärning a. Kolla tärning b. Jämför.
+Om olika, öka b.
+Om b > 5, öka a med 1 och därefter sätt b = (a+1)
+*/
 
 foreach ($dies as $dice) {
     echo '<p>' . $dice->getValue();
